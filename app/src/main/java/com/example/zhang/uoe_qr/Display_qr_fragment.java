829dff3af928a3c4fe2f12bf0d90bdbd18bd45cf
XZ_XuZhang <zhangxu852654@gmail.com>
2019-06-17 16:51:56 +0100

    added QR display

---
+++ app/src/main/java/com/example/zhang/uoe_qr/Display_qr_fragment.java
@@ -0,0 +1,30 @@
+package com.example.zhang.uoe_qr;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class Display_qr_fragment extends Fragment {
+
+    private ImageView qr_view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.display_qr_frag,container,false);
+
+        qr_view = v.findViewById(R.id.qr_view);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        qr_view.setImageResource(R.drawable.sample);
+    }
+}
--- app/src/main/java/com/example/zhang/uoe_qr/MainActivity.java
+++ app/src/main/java/com/example/zhang/uoe_qr/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.zhang.uoe_qr;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    public void onClickSignin(View v) {
+        Display_qr_fragment qr_frag = new Display_qr_fragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.qr_frag_container,qr_frag).addToBackStack(null).commit();
+        }
+
+
+
 }
