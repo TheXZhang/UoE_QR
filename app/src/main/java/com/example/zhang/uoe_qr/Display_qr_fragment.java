ce20d331922a47e94f7a166b1f16d9b9d3a9c495
XZ_XuZhang <zhangxu852654@gmail.com>
2019-06-21 15:42:47 +0100

    QR resources validation added

--- app/src/main/java/com/example/zhang/uoe_qr/Display_qr_fragment.java
+++ app/src/main/java/com/example/zhang/uoe_qr/Display_qr_fragment.java
@@ -1,6 +1,7 @@
 package com.example.zhang.uoe_qr;
 
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
@@ -17,6 +18,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -29,7 +34,11 @@ public class Display_qr_fragment extends Fragment {
     private Animation fadeInAnimation = new AlphaAnimation (0.0f, 1.0f);
     private int count=1;
     private TextView textTimer;
-    private int time=59;
+    private TextView errorText;
+    private Boolean b1 = false;
+    private Boolean b2 = false;
+    private Boolean b3 = false;
+
 
 
 
@@ -40,7 +49,7 @@ public class Display_qr_fragment extends Fragment {
         getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,WindowManager.LayoutParams.FLAG_SECURE);
         qr_view = v.findViewById(R.id.qr_view);
         textTimer = v.findViewById(R.id.timer);
-
+        errorText = v.findViewById(R.id.error_msg);
         return v;
     }
 
@@ -48,6 +57,70 @@ public class Display_qr_fragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        prelod();
+    }
+
+
+    public void prelod() {
+        Glide.with(getContext()).load("https://i.ibb.co/yfDw6y6/qr3.png").listener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                errorText.setText("Please check your network connection");
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                b1= true;
+                if (b1 && b2 && b3){
+                    errorText.setVisibility(View.GONE);
+                    start_animation();
+                }
+                return false;
+            }
+        }).preload();
+
+        Glide.with(getContext()).load("https://i.ibb.co/CQgYrWv/qr2.png").listener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                errorText.setText("Please check your network connection");
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                b2=true;
+                if (b1 && b2 && b3){
+                    errorText.setVisibility(View.GONE);
+                    start_animation();
+                }
+                return false;
+            }
+        }).preload();
+
+        Glide.with(getContext()).load("https://i.ibb.co/L1w3byW/qr1.png").listener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                errorText.setText("Please check your network connection");
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                b3=true;
+
+                if (b1 && b2 && b3){
+                    errorText.setVisibility(View.GONE);
+                    start_animation();
+                }
+                return false;
+            }
+        }).preload();
+
+    }
+
+
+    public void start_animation(){
         fadeOutAnimation.setDuration(2000);
         fadeInAnimation.setDuration(2000);
 
@@ -68,7 +141,7 @@ public class Display_qr_fragment extends Fragment {
             }
         });
 
-        fadeOutAnimation.setAnimationListener(new Animation.AnimationListener() {
+        fadeOutAnimation.setAnimationListener(new Animation.AnimationListener()  {
 
             @Override
             public void onAnimationStart(Animation animation) {
@@ -81,7 +154,7 @@ public class Display_qr_fragment extends Fragment {
             public void onAnimationEnd(Animation animation) {
                 switch (count){
                     case 1:
-                        Glide.with(getContext()).asBitmap().load("https://i.ibb.co/yfDw6y6/qr3.png").into(qr_view);
+                        Glide.with(getContext()).load("https://i.ibb.co/yfDw6y6/qr3.png").into(qr_view);
                         qr_view.startAnimation(fadeInAnimation);
                         Log.d("count1", "1");
                         count++;
@@ -108,6 +181,7 @@ public class Display_qr_fragment extends Fragment {
             }
         });
 
+
         qr_view.startAnimation(fadeOutAnimation);
 
         new CountDownTimer(60000*2, 1000){
@@ -124,6 +198,7 @@ public class Display_qr_fragment extends Fragment {
         }.start();
 
     }
+
 
 
 
